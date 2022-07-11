f823abbdc1f1979433e226718f39e01d67fabfd5
i.petrenko <i.petrenko@inspiritum.com>
2022-07-11 17:37:50 +0300

    deleted enemy

--- Assets/Scripts/Dino/Enemy/Spawn/PlaceProviders/ISpawnPlaceProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dino.Enemy.Spawn.Config;
-
-namespace Dino.Enemy.Spawn.PlaceProviders
-{
-    public interface ISpawnPlaceProvider
-    {
-        SpawnPlace GetSpawnPlace(EnemyWaveConfig waveConfig, int rangeTry);
-    }
-}
