7af5eb1c867c6f5b92a1271cb81c45f7a81e440a
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-07-19 13:58:55 +0300

    remove plugins / add dynamic cone renderer

--- Assets/Scripts/Dino/Units/Component/StaticConeFovRenderer.cs
+++ Assets/Scripts/Dino/Units/Component/StaticConeFovRenderer.cs
@@ -1,5 +1,4 @@
 ﻿using Dino.Extension;
-using EasyButtons;
 using UnityEngine;
 
 namespace Dino.Units.Component
@@ -11,59 +10,72 @@ namespace Dino.Units.Component
     public class StaticConeFovRenderer : MonoBehaviour, IFieldOfViewRenderer
     {
         private const int MIN_SEGMENTS_COUNT = 2;
-        private const int DEGREES_PER_SEGMENT = 3;
-        
         [SerializeField] private Material _material;
+        [SerializeField] private float _degreesPerSegment = 3;
 
-        private Mesh _mesh;
+        protected int _segmentsCount;
+        protected float _angle;
+        protected float _radius;
+        protected Mesh _mesh;
         
-        private void Awake()
+        protected virtual void Awake()
         {
-            var meshFilter = gameObject.RequireComponent<MeshFilter>();
-            var meshRenderer = gameObject.RequireComponent<MeshRenderer>();
-
-            meshRenderer.material = _material;
-            _mesh = meshFilter.mesh;
+            InitMesh();
         }
 
-        // test creation
-        [Button]
-        private void CreateTest()
+        private void InitMesh()
         {
-            Init(130, 10);
+            var renderer = gameObject.RequireComponent<MeshRenderer>();
+            renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            renderer.receiveShadows = false;
+            renderer.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off;
+            renderer.reflectionProbeUsage = UnityEngine.Rendering.ReflectionProbeUsage.Off;
+            renderer.allowOcclusionWhenDynamic = false;
+            renderer.material = _material;
+
+            var meshFilter = gameObject.RequireComponent<MeshFilter>();
+            _mesh = meshFilter.mesh;
         }
 
         public void Init(float angle, float radius)
         {
-            var segmentsCount = CalculateSegments(angle);
+            _segmentsCount = CalculateSegments(angle);
+            _angle = angle;
+            _radius = radius;
 
             _mesh.Clear();
-            _mesh.vertices = BuildVertices(segmentsCount, angle, radius);
-            _mesh.triangles = BuildTriangles(segmentsCount);
-            _mesh.normals = BuildNormals(segmentsCount);
+            _mesh.vertices = BuildVertices(_segmentsCount, angle, radius);
+            _mesh.triangles = BuildTriangles(_segmentsCount);
+            _mesh.normals = BuildNormals(_segmentsCount);
             _mesh.uv = BuildUvs(_mesh.vertices);
         }
 
         private int CalculateSegments(float angle)
         {
-            return (int) Mathf.Max(MIN_SEGMENTS_COUNT, angle / DEGREES_PER_SEGMENT);
+            return (int) Mathf.Max(MIN_SEGMENTS_COUNT, angle / _degreesPerSegment);
         }
 
-        private Vector3[] BuildVertices(int segmentsCount, float angle, float radius)
+        protected Vector3[] BuildVertices(int segmentsCount, float angle, float radius)
         {
-            var vertices = new Vector3[segmentsCount * 3];
+            var coneEdgeVertices = new Vector3[segmentsCount + 1];
             var segmentAngle = angle / segmentsCount;
             var currentAngle = -angle / 2;
-            for (int i = vertices.Length - 1; i >= 0; i -= 3)
+            for (int i = coneEdgeVertices.Length - 1; i >= 0; i -= 1)
             {
-                vertices[i] = GetVertPositionAtAngle(currentAngle, radius);
+                coneEdgeVertices[i] = GetVertPositionAtAngle(currentAngle, radius);
                 currentAngle += segmentAngle;
-                vertices[i - 1] = GetVertPositionAtAngle(currentAngle, radius);
+            }
+
+            var vertices = new Vector3[segmentsCount * 3];
+            for (int i = vertices.Length; i >= 1; i -= 3)
+            {
+                vertices[i - 1] = coneEdgeVertices[i / 3]; 
+                vertices[i - 2] = coneEdgeVertices[(i / 3) - 1]; 
             }
             return vertices;
         }
-
-        private Vector3 GetVertPositionAtAngle(float angle, float radius)
+        
+        protected virtual Vector3 GetVertPositionAtAngle(float angle, float radius)
         {
             var x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
             var z = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
