8de0e05aaf3d2e8a38ee5f24cbbb92029b2cc899
i.petrenko <i.petrenko@inspiritum.com>
2022-07-20 13:16:26 +0300

    fixed CreateNewId

--- Assets/Scripts/Dino/Inventory/Service/InventoryService.cs
+++ Assets/Scripts/Dino/Inventory/Service/InventoryService.cs
@@ -5,6 +5,7 @@ using Dino.Location;
 using Dino.Weapon.Config;
 using Dino.Weapon.Model;
 using Feofun.Config;
+using ModestTree;
 using SuperMaxim.Core.Extensions;
 using UniRx;
 using Zenject;
@@ -36,9 +37,10 @@ namespace Dino.Inventory.Service
         {
             _weaponConfigs.ForEach(it => Add(it.Id.ToString()));
         }
+
         public bool HasInventory() => _repository.Exists() && _inventory.HasValue && _inventory.Value != null;
         public bool Contains(ItemId id) => Inventory.Contains(id);
-        
+
         public void Add(string itemName)
         {
             var inventory = Inventory;
@@ -46,18 +48,17 @@ namespace Dino.Inventory.Service
             inventory.Add(itemId);
             Set(inventory);
         }
-        
+
         public void Remove(ItemId id)
         {
             var inventory = Inventory;
             inventory.Remove(id);
             Set(inventory);
         }
+
         public ItemId GetLast(string itemName)
         {
-            var itemId = _inventory.Value.Items.Where(it => it.Name == itemName)
-                                   .OrderBy(it => it.Number)
-                                   .LastOrDefault();
+            var itemId = _inventory.Value.Items.Where(it => it.Name == itemName).OrderBy(it => it.Number).LastOrDefault();
             if (itemId == null) {
                 throw new NullReferenceException($"Error getting last item, inventory doesn't contain item name:= {itemName}");
             }
@@ -66,9 +67,14 @@ namespace Dino.Inventory.Service
 
         private ItemId CreateNewId(string itemName)
         {
-            var number = Inventory.Items.Count(it => it.Name == itemName) + 1;
+            var items = Inventory.Items.Where(it => it.Name == itemName);
+            if (items.IsEmpty()) {
+                return ItemId.Create(itemName, 1);
+            }
+            var number = items.Max(it => it.Number) + 1;
             return ItemId.Create(itemName, number);
         }
+
         private void Set(Model.Inventory model)
         {
             _repository.Set(model);
@@ -80,7 +86,5 @@ namespace Dino.Inventory.Service
             _inventory.SetValueAndForceNotify(null);
             _repository.Delete();
         }
-
- 
     }
 }
