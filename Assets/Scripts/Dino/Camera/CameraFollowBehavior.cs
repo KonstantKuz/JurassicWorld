c540a6b85ddb97416ca5bff40192f5ff444ffa75
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-08-02 10:55:12 +0300

    refactor cam behaviour

--- Assets/Scripts/Dino/Camera/CameraFollowBehavior.cs
+++ Assets/Scripts/Dino/Camera/CameraFollowBehavior.cs
@@ -24,10 +24,14 @@ namespace Dino.Camera
         private void Update()
         {
             var nextPosition = transform.position - _distance * Camera.transform.forward;
+            Camera.transform.position = ClampByLevel(nextPosition);
+        }
+
+        private Vector3 ClampByLevel(Vector3 nextPosition)
+        {
             if (_world.Level == null)
             {
-                Camera.transform.position = nextPosition;
-                return;
+                return nextPosition;
             }
             
             var levelBounds = CurrentLevel.GetBounds();
@@ -40,8 +44,8 @@ namespace Dino.Camera
             nextPosition.z = Mathf.Clamp(nextPosition.z,
                 cameraOffset.z + levelBounds.center.z - levelBounds.extents.z + _offsetFromLevelEdge,
                 cameraOffset.z + levelBounds.center.z + levelBounds.extents.z - _offsetFromLevelEdge);
-            
-            Camera.transform.position = nextPosition;
+
+            return nextPosition;
         }
     }
 }
