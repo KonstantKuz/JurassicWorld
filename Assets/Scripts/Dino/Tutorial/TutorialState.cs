a328f8b80fc36ce7858dbb0832d3e2fde18e35c0
i.petrenko <i.petrenko@inspiritum.com>
2022-08-15 16:13:31 +0300

    fixed TutorialState

--- Assets/Scripts/Dino/Tutorial/TutorialState.cs
+++ Assets/Scripts/Dino/Tutorial/TutorialState.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Dino.Tutorial
 {
     public class TutorialState
     {
+        [JsonProperty]
         public readonly HashSet<string> CompletedStages = new HashSet<string>();
     }
 }
