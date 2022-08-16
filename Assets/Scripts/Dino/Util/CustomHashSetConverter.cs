84479a6f9e479b2bda320b1bdd7c3705a70dd884
i.petrenko <i.petrenko@inspiritum.com>
2022-08-16 15:17:50 +0300

    fixed CustomHashSetConverter

--- Assets/Scripts/Dino/Util/CustomHashSetConverter.cs
+++ Assets/Scripts/Dino/Util/CustomHashSetConverter.cs
@@ -15,16 +15,14 @@ namespace Dino.Util
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            var jObject = JObject.Load(reader);
-            var list = jObject.Property(typeof(T).Name)?.Value.Children().Select(it => it.ToObject(typeof(T))).Cast<T>();
-            return new HashSet<T>(list);
+            return new HashSet<T>(JArray.Load(reader).Values<T>());
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var hashSet = (HashSet<T>) value;
-            var jo = new JObject(new JProperty(typeof(T).Name, hashSet.Select(s => JToken.FromObject(s))));
-            jo.WriteTo(writer);
+            var jArray = new JArray(hashSet.Select(s => JToken.FromObject(s)));
+            jArray.WriteTo(writer);
         }
     }
 }
