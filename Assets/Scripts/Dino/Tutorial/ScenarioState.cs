8c0046714c3000112193b8ec856a092eb74fcebe
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-09-05 19:18:43 +0300

    split state

---
+++ Assets/Scripts/Dino/Tutorial/ScenarioState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Dino.Util;
+using Newtonsoft.Json;
+
+namespace Dino.Tutorial
+{
+    public class ScenarioState
+    {
+        public bool IsCompleted;
+        [JsonConverter(typeof(CustomHashSetConverter<string>))]
+        public HashSet<string> CompletedSteps = new HashSet<string>();
+    }
+}
--- Assets/Scripts/Dino/Tutorial/TutorialState.cs
+++ Assets/Scripts/Dino/Tutorial/TutorialState.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections.Generic;
-using Dino.Util;
-using Newtonsoft.Json;
 
 namespace Dino.Tutorial
 {
     public class TutorialState
     {
         public readonly Dictionary<TutorialScenarioId, ScenarioState> Scenarios = new Dictionary<TutorialScenarioId, ScenarioState>();
-    }
-    
-    public class ScenarioState
-    {
-        public bool IsCompleted;
-        [JsonConverter(typeof(CustomHashSetConverter<string>))]
-        public HashSet<string> CompletedSteps = new HashSet<string>();
     }
 }
