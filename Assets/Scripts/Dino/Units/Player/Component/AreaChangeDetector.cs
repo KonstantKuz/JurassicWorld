14704b38cdf6688d3fdb84e606e8a49bfd5ebd11
Anatoly Ahmedov <ludd@bk.ru>
2022-09-26 10:41:59 +0300

    add hidden navmesh area and detect area change

---
+++ Assets/Scripts/Dino/Units/Player/Component/AreaChangeDetector.cs
@@ -0,0 +1,23 @@
+﻿using Logger.Extension;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Dino.Units.Player.Component
+{
+    public class AreaChangeDetector : MonoBehaviour
+    {
+        private int _lastAreaMask = 0;
+        
+        private void Update()
+        {
+            if (!NavMesh.SamplePosition(transform.position, out var hit, 0.1f, NavMesh.AllAreas))
+            {
+                return;
+            }
+            if (hit.mask == _lastAreaMask) return;
+            
+            this.Logger().Debug($"Area changed: {hit.mask}");
+            _lastAreaMask = hit.mask;
+        }
+    }
+}
