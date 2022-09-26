05e3f6b51d0fb2f658aa76f56cc2eab451c1222c
Anatoly Ahmedov <ludd@bk.ru>
2022-09-26 18:23:55 +0300

    AreaChangeDetector.cs - add reactive property

--- Assets/Scripts/Dino/Units/Player/Component/AreaChangeDetector.cs
+++ Assets/Scripts/Dino/Units/Player/Component/AreaChangeDetector.cs
@@ -1,4 +1,4 @@
-﻿using Logger.Extension;
+﻿using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,8 +6,19 @@ namespace Dino.Units.Player.Component
 {
     public class AreaChangeDetector : MonoBehaviour
     {
-        private int _lastAreaMask = 0;
+        private const int GRASS_AREA_MASK = 8;
         
+        public enum AreaType
+        {
+            Default,
+            Grass
+        };
+
+        private int _lastAreaMask = 0;
+        private readonly ReactiveProperty<AreaType> _currentAreaType = new ReactiveProperty<AreaType>();
+
+        public IReadOnlyReactiveProperty<AreaType> CurrentAreaType => _currentAreaType;
+
         private void Update()
         {
             if (!NavMesh.SamplePosition(transform.position, out var hit, 0.1f, NavMesh.AllAreas))
@@ -15,9 +26,17 @@ namespace Dino.Units.Player.Component
                 return;
             }
             if (hit.mask == _lastAreaMask) return;
-            
-            this.Logger().Debug($"Area changed: {hit.mask}");
             _lastAreaMask = hit.mask;
+            _currentAreaType.Value = GetAreaType(_lastAreaMask);            
+        }
+
+        private static AreaType GetAreaType(int areaMask)
+        {
+            return areaMask switch
+            {
+                GRASS_AREA_MASK => AreaType.Grass,
+                _ => AreaType.Default
+            };
         }
     }
 }
