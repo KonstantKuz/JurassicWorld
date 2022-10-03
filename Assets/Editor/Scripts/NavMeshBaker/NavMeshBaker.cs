ffbeb9058377b61f48f20569187a839898ace394
i.petrenko <i.petrenko@inspiritum.com>
2022-10-03 10:20:40 +0300

    refactoring NavMeshBaker

--- Assets/Editor/Scripts/NavMeshBaker/NavMeshBaker.cs
+++ Assets/Editor/Scripts/NavMeshBaker/NavMeshBaker.cs
@@ -1,86 +1,112 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using SuperMaxim.Core.Extensions;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace Editor.Scripts.NavMeshBaker
 {
-    public class NavMeshBaker : EditorWindow
+    public class NavMeshBaker
     {
-        private string _prefab = "Assets/Resources/Content/Level/1-2/scene.prefab";
+        private const string PREFAB_ASSETS_FILTER = "t:Prefab";
+        private const string NAV_MESH_DATA_PROPERTY_NAME = "m_NavMeshData";
 
-        [MenuItem("App/Build NavMeshes")]
-        public static void ShowWindow()
+        public static void BakeAllNavMeshesInFolder(string folderPath)
         {
-            GetWindow(typeof(NavMeshBaker));
+            GetAllAssetPathInFolder(folderPath, PREFAB_ASSETS_FILTER).ForEach(BakeNavMeshesInPrefab);
         }
 
-        private void OnGUI()
+        private static IEnumerable<string> GetAllAssetPathInFolder(string folderPath, string filter)
         {
-            _prefab = EditorGUILayout.TextField("Prefab", _prefab);
+            return AssetDatabase.FindAssets(filter, new[] {folderPath}).Select(AssetDatabase.GUIDToAssetPath);
+        }
 
-            if (GUILayout.Button("Build NavMeshes")) {
-                BakeAllNavmeshInFolder(_prefab);
+        private static void BakeNavMeshesInPrefab(string prefabAssetPath)
+        {
+            var prefab = PrefabUtility.LoadPrefabContents(prefabAssetPath);
+            var prefabSurfaces = prefab.GetComponentsInChildren<NavMeshSurface>();
+
+            if (prefabSurfaces.IsNullOrEmpty()) {
+                PrefabUtility.UnloadPrefabContents(prefab);
+                return;
             }
-        }
+            var bakedNavMeshCount = 0;
 
-        public void BakeAllNavmeshInFolder(string assetPath)
-        {
-            GameObject prefab = PrefabUtility.LoadPrefabContents(assetPath);
-            NavMeshSurface[] surfaces = prefab.GetComponentsInChildren<NavMeshSurface>();
-
-            var completedNavMeshCount = 0;
-            for (int index = 0; index < surfaces.Length; index++) {
-          
-                var navMeshSurface = surfaces[index];
-                var data = InitializeBakeData(navMeshSurface);
-                var asyncOperation = navMeshSurface.UpdateNavMesh(data);
-                asyncOperation.completed += (operation) => {
-                    SetNavMeshData(navMeshSurface, data);
-                    CreateNavMeshAsset(navMeshSurface);
-                    PrefabUtility.SaveAsPrefabAsset(prefab, assetPath);
-                    completedNavMeshCount++;
-                    if (completedNavMeshCount == surfaces.Length) {
-                        PrefabUtility.UnloadPrefabContents(prefab); 
+            for (int index = 0; index < prefabSurfaces.Length; index++) {
+                var surface = prefabSurfaces[index];
+                var navMeshData = InitializeBakeData(surface);
+                var updateNavMeshAsync = surface.UpdateNavMesh(navMeshData);
+                
+                
+                void OnNavMeshAsyncUpdated(AsyncOperation operation)
+                {
+                    updateNavMeshAsync.completed -= OnNavMeshAsyncUpdated;
+                    UpdateNavMeshData(surface, navMeshData);
+                    bakedNavMeshCount++;
+                    if (bakedNavMeshCount == prefabSurfaces.Length) {
+                        SavePrefab(prefab, prefabAssetPath);
                     }
-                };
+                }
+                
+                updateNavMeshAsync.completed += OnNavMeshAsyncUpdated;
+
             }
-        }
-        private static void CreateNavMeshAsset(NavMeshSurface surface)
-        {
-            var targetPath = GetAndEnsureTargetPath(surface);
-            
-      
-            if (!Directory.Exists(Path.Combine(targetPath, "NavMesh")))
-            {
-                AssetDatabase.CreateFolder(targetPath, "NavMesh");
-            }
-            var combinedAssetPath = Path.Combine(targetPath, "NavMesh", "NavMesh-" + surface.name + ".asset");
-            combinedAssetPath = AssetDatabase.GenerateUniqueAssetPath(combinedAssetPath);
-            AssetDatabase.CreateAsset(surface.navMeshData, combinedAssetPath);
         }
 
-        static void SetNavMeshData(NavMeshSurface navSurface, NavMeshData navMeshData)
+        private static void UpdateNavMeshData(NavMeshSurface surface, NavMeshData navMeshData)
         {
-            var so = new SerializedObject(navSurface);
-            var navMeshDataProperty = so.FindProperty("m_NavMeshData");
+            DeleteOldNavMeshData(surface);
+            SetNewNavMeshData(surface, navMeshData);
+            CreateNavMeshDataAsset(surface);
+        }
+
+        private static void SavePrefab(GameObject prefab, string prefabAssetPath)
+        {
+            PrefabUtility.SaveAsPrefabAsset(prefab, prefabAssetPath);
+            PrefabUtility.UnloadPrefabContents(prefab);
+        }
+
+        private static void DeleteOldNavMeshData(NavMeshSurface surface)
+        {
+            var navMeshData = surface.navMeshData;
+            if (navMeshData == null) {
+                return;
+            }
+            AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(navMeshData));
+        }
+
+        private static void CreateNavMeshDataAsset(NavMeshSurface surface)
+        {
+            var surfacePrefabPath = Path.GetDirectoryName(surface.gameObject.scene.path);
+
+            var navMeshDataFolderPath = Path.Combine(surfacePrefabPath, "NavMesh");
+
+            if (!Directory.Exists(navMeshDataFolderPath)) {
+                AssetDatabase.CreateFolder(surfacePrefabPath, "NavMesh");
+            }
+
+            var navMeshDataPath = Path.Combine(navMeshDataFolderPath, "NavMesh-" + surface.name + ".asset");
+
+            navMeshDataPath = AssetDatabase.GenerateUniqueAssetPath(navMeshDataPath);
+            AssetDatabase.CreateAsset(surface.navMeshData, navMeshDataPath);
+        }
+
+        private static void SetNewNavMeshData(NavMeshSurface surface, NavMeshData navMeshData)
+        {
+            var serializedSurface = new SerializedObject(surface);
+            var navMeshDataProperty = serializedSurface.FindProperty(NAV_MESH_DATA_PROPERTY_NAME);
             navMeshDataProperty.objectReferenceValue = navMeshData;
-            so.ApplyModifiedPropertiesWithoutUndo();
+            serializedSurface.ApplyModifiedPropertiesWithoutUndo();
         }
 
-        static NavMeshData InitializeBakeData(NavMeshSurface surface)
+        private static NavMeshData InitializeBakeData(NavMeshSurface surface)
         {
             var emptySources = new List<NavMeshBuildSource>();
             var emptyBounds = new Bounds();
-            return UnityEngine.AI.NavMeshBuilder.BuildNavMeshData(surface.GetBuildSettings(), emptySources, emptyBounds, surface.transform.position,
-                                                                  surface.transform.rotation);
-        }
-
-        static string GetAndEnsureTargetPath(NavMeshSurface surface)
-        {
-            var activeScenePath = surface.gameObject.scene.path;
-            return Path.Combine(Path.GetDirectoryName(activeScenePath));
+            return NavMeshBuilder.BuildNavMeshData(surface.GetBuildSettings(), emptySources, emptyBounds, surface.transform.position,
+                                                   surface.transform.rotation);
         }
     }
 }
