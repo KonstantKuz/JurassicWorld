588f1d8ea33e1d5f79f43762e4a5dcd15272df28
Anatoly Ahmedov <ludd@bk.ru>
2022-10-07 10:56:53 +0300

    fixes

--- Assets/Scripts/Dino/Location/Level/Service/LevelService.cs
+++ Assets/Scripts/Dino/Location/Level/Service/LevelService.cs
@@ -20,8 +20,8 @@ namespace Dino.Location.Level.Service
         [Inject] private ConstantsConfig _constantsConfig;
 
         private PlayerProgress PlayerProgress => _playerProgressService.Progress;
-        
-        public string CurrentLevelId => GetLevelId(PlayerProgress.WinCount);
+
+        public string CurrentLevelId => "Level2-2";//GetLevelId(PlayerProgress.WinCount);
         public List<Level> Levels => _levels ??= _worldObjectFactory.GetPrefabComponents<Level>();
 
         private string GetLevelId(int winCount)
