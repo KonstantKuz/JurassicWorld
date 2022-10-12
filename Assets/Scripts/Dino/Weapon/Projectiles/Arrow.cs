23b06ac5389c02a6e5f5dbec47922654df4ae767
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-10-12 14:13:41 +0300

    fix arrow target follow

--- Assets/Scripts/Dino/Weapon/Projectiles/Arrow.cs
+++ Assets/Scripts/Dino/Weapon/Projectiles/Arrow.cs
@@ -9,24 +9,31 @@ namespace Dino.Weapon.Projectiles
 {
     public sealed class Arrow : Projectile
     {
+        [SerializeField] private bool _followTarget;
+        [SerializeField] private float _rotationSpeed;
         [SerializeField] private float _maxLifeTime;
+        [SerializeField] private float _initialCourseTime;
         [SerializeField] private float _hitStuckTime;
         [SerializeField] private KickbackReactionParams _kickbackParams;
 
-        private bool _isStuck;
-        
+        private bool _isAttached;
+
+        private ITarget _target;
+
         private float TimeLeft { get; set; }
+
+        private float LifeTime => _maxLifeTime - TimeLeft;
         
         public override void Launch(ITarget target, IWeaponModel model, Action<GameObject> hitCallback)
         {
             base.Launch(target, model, hitCallback);
-            _isStuck = false;
+            _isAttached = false;
+            SetTarget(target);
             TimeLeft = _maxLifeTime;
         }
-        
         private void Update()
         {
-            if (!_isStuck)
+            if (!_isAttached)
             {
                 UpdatePosition();
             }
@@ -37,9 +44,28 @@ namespace Dino.Weapon.Projectiles
             }
         }
 
+        private void SetTarget(ITarget target)
+        {
+            Assert.IsNull(_target, "we are currently supporting only one call to SetTarget on launch");
+            Assert.IsNotNull(target);
+            _target = target;
+            _target.OnTargetInvalid += ClearTarget;
+        }
+
         private void UpdatePosition()
         {
             transform.position += transform.forward * Speed * Time.deltaTime;
+
+            if (!_followTarget || !_target.IsTargetValidAndAlive())
+            {
+                return;
+            }
+            
+            if (LifeTime >= _initialCourseTime)
+            {
+                var lookRotation = Quaternion.LookRotation(_target.Center.position - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * _rotationSpeed);
+            }
         }
 
         protected override void TryHit(GameObject target, Vector3 hitPos, Vector3 collisionNorm)
@@ -47,19 +73,29 @@ namespace Dino.Weapon.Projectiles
             base.TryHit(target, hitPos, collisionNorm);
             KickbackReaction.TryExecuteOn(target, -collisionNorm, _kickbackParams);
             AttachToTarget(target);
+            ClearTarget();
             TimeLeft = _hitStuckTime;
         }
 
         private void AttachToTarget(GameObject target)
         {
-            _isStuck = true;
+            _isAttached = true;
             transform.SetParent(target.transform);
         }
 
         private void Destroy()
         {
+            ClearTarget();
             HitCallback = null;
             Destroy(gameObject);
+        }
+
+        private void ClearTarget()
+        {
+            if (_target != null) {
+                _target.OnTargetInvalid -= ClearTarget;
+                _target = null;
+            }
         }
     }
 }
